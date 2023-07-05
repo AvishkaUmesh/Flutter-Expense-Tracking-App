8b2c489116f1b9d37163c2d1929c36f963e819f1
Avishka Umesh <umesh.avishka@gmail.com>
2023-07-05 11:02:28 +0530

    added ExpensesBucket class

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -37,3 +37,27 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpensesBucket {
+  ExpensesBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpensesBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
